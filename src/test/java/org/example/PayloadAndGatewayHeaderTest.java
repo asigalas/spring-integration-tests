e7d67db1c30ed779eed563ce874e264282e7d4de
Frank Sigalas <antonios.sigalas@nokia.com>
2018-12-13 14:11:41 +0200

    @Payload and @GatewayHeader example

--- src/test/java/org/example/PayloadAndGatewayHeaderTest.java
+++ src/test/java/org/example/PayloadAndGatewayHeaderTest.java
@@ -1,24 +1,14 @@
 package org.example;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.example.PayloadAndGatewayHeader.ArithmeticGateway;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.integration.dsl.MessageChannels;
-import org.springframework.integration.support.MessageBuilder;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.PollableChannel;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@ContextConfiguration(classes = {
-//        PayloadAndGatewayHeaderTest.ContextConfiguration.class//,
-//        //PayloadAndGatewayHeader.class
-//})
 @Import(PayloadAndGatewayHeader.class)
 @RunWith(SpringRunner.class)
 public class PayloadAndGatewayHeaderTest {
